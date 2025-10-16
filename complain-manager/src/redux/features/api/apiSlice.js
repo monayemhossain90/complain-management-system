4ac3ac51f42d333880cc7cb9d41e381f3dd23ac9
monayemhossain90 <monayemhossain1990@gmail.com>
2025-10-16 14:50:27 +0600

    updated backend url

--- complain-manager/src/redux/features/api/apiSlice.js
+++ complain-manager/src/redux/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import {getToken} from "../../../helper/SessionHelper.js";
 
 const baseQuery = fetchBaseQuery({
     
-     baseUrl: "https://complain-management-system-alpha.vercel.app/api",
+     baseUrl: "https://complain-management-server.vercel.app/api",
      
     //   baseUrl: "http://localhost:5000/api",
     prepareHeaders: async (headers, {getState, endpoint}) =>{
